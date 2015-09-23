ffe212a3703ebb3fe5f03131598b6603ba9803e3
Sean Moss <semo0788@colorado.edu>
2015-09-23 11:31:39 -0600

    PS3 transform helpers and cube drawing

--- ps3/render_helper.c
+++ ps3/render_helper.c
@@ -13,11 +13,29 @@
 
 #define LEN 8192  /* Maximum length of text string. */
 
-/* Render world axes with the given sizes */
-void rh_draw_world_axes(double x, double y, double z)
+/* Internal helper functions */
+static void _apply_transforms(VEC3 pos, VEC3 scale, VEC3 rot)
+{
+  glScaled(scale.x, scale.y, scale.z);
+  glRotated(rot.x, 1.0, 0.0, 0.0);
+  glRotated(rot.y, 0.0, 1.0, 0.0);
+  glRotated(rot.z, 0.0, 0.0, 1.0);
+  glTranslated(pos.x, pos.y, pos.z);
+}
+static void _reset_transforms()
 {
   glPushMatrix();
   glLoadIdentity();
+}
+static void _reload_transforms()
+{
+  glPopMatrix();
+}
+
+/* Render world axes with the given sizes */
+void rh_draw_world_axes(double x, double y, double z)
+{
+  _reset_transforms();
 
   glBegin(GL_LINES);
     glColor3d(1.0, 1.0, 1.0);
@@ -43,7 +61,7 @@ void rh_draw_world_axes(double x, double y, double z)
   rh_draw_raster_text(ys, "Y");
   rh_draw_raster_text(zs, "Z");
 
-  glPopMatrix();
+  _reload_transforms();
 }
 
 /* Render text based on raster position */
@@ -78,4 +96,56 @@ void rh_draw_window_text(VEC2 pos, const char *format, ...)
   /* Display the characters one at a time at the current raster position */
   while (*ch)
     glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, *ch++);
+}
+
+/* Render a multi-colored cube at the given position, scale, and rotation. */
+void rh_draw_cube(VEC3 pos, VEC3 scale, VEC3 rot)
+{
+  _reset_transforms();
+  _apply_transforms(pos, scale, rot);
+
+  glBegin(GL_QUADS);
+
+    /* +X face */
+    glColor3i(1, 0, 0); /* Red */
+    glVertex3i(1, 1, 1);
+    glVertex3i(1, 1, -1);
+    glVertex3i(1, -1, -1);
+    glVertex3i(1, -1, 1);
+    /* -X face */
+    glColor3i(0, 1, 0); /* Green */
+    glVertex3i(-1, 1, -1);
+    glVertex3i(-1, 1, 1);
+    glVertex3i(-1, -1, 1);
+    glVertex3i(-1, -1, -1);
+
+    /* +Y face */
+    glColor3i(0, 0, 1); /* Blue */
+    glVertex3i(-1, 1, -1);
+    glVertex3i(1, 1, -1);
+    glVertex3i(1, 1, 1);
+    glVertex3i(-1, 1, 1);
+    /* -Y face */
+    glColor3i(1, 1, 0); /* Yellow */
+    glVertex3i(1, -1, -1);
+    glVertex3i(-1, -1, -1);
+    glVertex3i(-1, -1, 1);
+    glVertex3i(1, -1, 1);
+
+    /* +Z face */
+    glColor3i(1, 0, 1); /* Magenta */
+    glVertex3i(-1, 1, 1);
+    glVertex3i(1, 1, 1);
+    glVertex3i(1, -1, 1);
+    glVertex3i(-1, -1, 1);
+    /* -Z face */
+    glColor3i(0, 1, 1); /* Cyan */
+    glVertex3i(1, 1, -1);
+    glVertex3i(-1, 1, -1);
+    glVertex3i(-1, -1, -1);
+    glVertex3i(1, -1, -1);
+
+  glEnd();
+
+  _reload_transforms();
 }
--- ps3/render_helper.h
+++ ps3/render_helper.h
@@ -25,6 +25,9 @@ typedef struct
   double w;
 } VEC4, COLOR4;
 
+/* Create a look-at view matrix */
+void rh_camera_look_at(VEC3 target, VEC3 position);
+
 /* Render world axes with the given sizes */
 void rh_draw_world_axes(double x, double y, double z);
 
@@ -32,5 +35,8 @@ void rh_draw_world_axes(double x, double y, double z);
 void rh_draw_raster_text(VEC3 pos, const char *format, ...);
 /* Render text based on window position */
 void rh_draw_window_text(VEC2 pos, const char *format, ...);
+
+/* Render a multi-colored cube at the given position, scale, and rotation. */
+void rh_draw_cube(VEC3 pos, VEC3 scale, VEC3 rot);
 
 #endif // RENDER_HELPER_H_
